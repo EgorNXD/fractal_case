bb27c6649b7eecb603220c491cb1722a8e2d54f3
EgorNXD <e.nechaev@g.nsu.ru>
2023-11-28 22:35:27 +0700

    5

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ def sf_koch(order, size):
     turtle.goto(-400, 200)
     turtle.down()
 
-    koch(order, size)
+    koch(order, size/3)
     turtle.right(120)
-    koch(order, size)
+    koch(order, size/3)
     turtle.right(120)
-    koch(order, size)
+    koch(order, size/3)
     turtle.seth(0)
 
 
@@ -74,7 +74,7 @@ def line_mink(order, size):
     turtle.goto(-400, 0)
     turtle.down()
 
-    mink(size, order)
+    mink(order, size)
     turtle.seth(0)
 
 def sun_mink(order, size):
@@ -96,6 +96,57 @@ def sun_mink(order, size):
     turtle.seth(0)
 
 
+def ice_1(order, size):
+    if order == 0:
+        turtle.forward(size)
+    else:
+        ice_1(order - 1, size / 3)
+        turtle.left(90)
+        ice_1(order-1, size / 6)
+        turtle.right(180)
+        ice_1(order - 1, size / 6)
+        turtle.left(90)
+        ice_1(order - 1, size / 3)
+
+def line_ice_1(order, size):
+    turtle.clearscreen()
+    turtle.speed(100)
+    turtle.pencolor("black")
+    turtle.pensize(2)
+    turtle.up()
+    turtle.goto(-400, 0)
+    turtle.down()
+
+    ice_1(order, size)
+    turtle.seth(0)
+
+def ice_2(order, size):
+    if order == 0:
+        turtle.forward(size)
+    else:
+        ice_1(order - 1, size / 3)
+        turtle.left(90)
+        ice_1(order-1, size / 6)
+        turtle.right(180)
+        ice_1(order - 1, size / 6)
+        turtle.left(90)
+        ice_1(order - 1, size / 3)
+
+def line_ice_2(order, size):
+    turtle.clearscreen()
+    turtle.speed(100)
+    turtle.pencolor("black")
+    turtle.pensize(2)
+    turtle.up()
+    turtle.goto(-400, 0)
+    turtle.down()
+
+    ice_2(order, size)
+    turtle.seth(0)
+
+line_ice_2(0, 500)
+#sun_mink(2, 1000)
+#line_koch(3,100)
 
 turtle.up()
 turtle.hideturtle()
